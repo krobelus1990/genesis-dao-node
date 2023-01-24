a2b30d0b5d627db1b5a5925e82d114f44469c7d5
Marijn Schouten <mas@deep-ink.ventures>
2023-01-24 13:08:28 +0100

    Initial working test for proposal creation

--- pallets/dao-votes/src/tests.rs
+++ pallets/dao-votes/src/tests.rs
@@ -4,11 +4,23 @@ use frame_support::assert_ok;
 #[test]
 fn it_creates_a_proposal() {
 	new_test_ext().execute_with(|| {
-		// assert_ok!(DaoVotes::create_proposal(
-		// 	RuntimeOrigin::signed(1),
-		// 	b"TEST_DAO".to_vec(),
-		// 	b"TEST_proposal".to_vec()
-		// ));
+		// preparation: create a DAO
+		assert_ok!(DaoCore::create_dao(
+			RuntimeOrigin::signed(1),
+			b"TEST".to_vec(),
+			b"TEST DAO".to_vec()
+		));
+		// preparation: issue token
+		assert_ok!(DaoCore::issue_token(RuntimeOrigin::signed(1), b"TEST".to_vec(), 1000));
+
+		// actual test
+		assert_ok!(DaoVotes::create_proposal(
+			RuntimeOrigin::signed(1),
+			b"TEST".to_vec(),
+			b"TEST_proposal".to_vec()
+		));
+
+		// creating a proposal should reserve DAO tokens
 		//assert_eq!(Balances::reserved_balance(1), 2);
 	});
 }
