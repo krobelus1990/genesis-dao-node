44c1ba2a1d1ca3bcae007ce5001added7cdfbdae
Marijn Schouten <mas@deep-ink.ventures>
2023-01-25 10:48:28 +0100

    Test for failure modes of proposal creation

--- pallets/dao-votes/src/tests.rs
+++ pallets/dao-votes/src/tests.rs
@@ -1,24 +1,34 @@
-use crate::mock::*;
-use frame_support::assert_ok;
+use crate::{mock::*, Error};
+use frame_support::{assert_noop, assert_ok};
+use pallet_dao_core::Error as DaoError;
 
 #[test]
 fn it_creates_a_proposal() {
 	new_test_ext().execute_with(|| {
-		// preparation: create a DAO
-		assert_ok!(DaoCore::create_dao(
-			RuntimeOrigin::signed(1),
-			b"TEST".to_vec(),
-			b"TEST DAO".to_vec()
-		));
-		// preparation: issue token
-		assert_ok!(DaoCore::issue_token(RuntimeOrigin::signed(1), b"TEST".to_vec(), 1000));
+		let dao_id = b"TEST".to_vec();
+		let dao_name = b"TEST DAO".to_vec();
+		let prop_id = b"TEST_proposal".to_vec();
 
-		// actual test
-		assert_ok!(DaoVotes::create_proposal(
-			RuntimeOrigin::signed(1),
-			b"TEST".to_vec(),
-			b"TEST_proposal".to_vec()
-		));
+		// cannot create a proposal without a DAO
+		assert_noop!(
+			DaoVotes::create_proposal(RuntimeOrigin::signed(1), dao_id.clone(), prop_id.clone()),
+			DaoError::<Test>::DaoDoesNotExist
+		);
+
+		// preparation: create a DAO
+		assert_ok!(DaoCore::create_dao(RuntimeOrigin::signed(1), dao_id.clone(), dao_name));
+
+		// cannot create a proposal without DAO tokens existing (because they need to be reserved)
+		assert_noop!(
+			DaoVotes::create_proposal(RuntimeOrigin::signed(1), dao_id.clone(), prop_id.clone()),
+			Error::<Test>::DaoTokenNotYetIssued
+		);
+
+		// preparation: issue token
+		assert_ok!(DaoCore::issue_token(RuntimeOrigin::signed(1), dao_id.clone(), 1000));
+
+		// test creating a proposal
+		assert_ok!(DaoVotes::create_proposal(RuntimeOrigin::signed(1), dao_id, prop_id));
 
 		// creating a proposal should reserve DAO tokens
 		//assert_eq!(Balances::reserved_balance(1), 2);
