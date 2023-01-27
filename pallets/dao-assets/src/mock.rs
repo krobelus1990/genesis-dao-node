081c35ee73ec4528c336200245901590b9b99928
Marijn Schouten <mas@deep-ink.ventures>
2023-01-27 12:33:25 +0100

    clippy autofixes for dao-assets

--- pallets/dao-assets/src/mock.rs
+++ pallets/dao-assets/src/mock.rs
@@ -127,7 +127,7 @@ pub(crate) fn clear_frozen_balance(asset: u32, who: u64) {
 }
 
 pub(crate) fn hooks() -> Vec<Hook> {
-	Hooks::get().clone()
+	Hooks::get()
 }
 
 pub(crate) fn take_hooks() -> Vec<Hook> {
@@ -156,7 +156,7 @@ pub(crate) fn new_test_ext() -> sp_io::TestExternalities {
 
 	let mut ext: sp_io::TestExternalities = storage.into();
 	// Clear thread local vars for https://github.com/paritytech/substrate/issues/10479.
-	ext.execute_with(|| take_hooks());
+	ext.execute_with(take_hooks);
 	ext.execute_with(|| System::set_block_number(1));
 	ext
 }
